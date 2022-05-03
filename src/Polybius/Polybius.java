73d611d3d2906f2d24844d5f7c6419e198489854
Janssendel <61640498+Janssendel@users.noreply.github.com>
2022-05-03 12:29:09 +0700

    this is a lot- look, im coming back.

--- src/Polybius/Polybius.java
+++ src/Polybius/Polybius.java
@@ -1,37 +1,15 @@
-package example;
+package Polybius;
 
 import arc.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
+import mindustry.ui.fragments.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.type.*;
 import mindustry.ui.dialogs.*;
+import Polybius.content.*;
+import Polybius.graphics.*;
 
-public class Polybius extends Mod{
-
-    public Polybius(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                dialog.cont.add("froge").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see froge", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
-    }
-
-    @Override
-    public void loadContent(){
-        Log.info("Loading Polybius.");
-    }
-
-}
